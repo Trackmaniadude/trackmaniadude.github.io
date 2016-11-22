c76204234b030821791e3f6583c91c9c06f186c1
Trackmaniadude <alex108@tringali.org>
2016-11-22 18:56:31 -0500

    Update gameController.js

--- scripts/gameController.js
+++ scripts/gameController.js
@@ -932,7 +932,7 @@ function drawTank() {
 		ctx.save();
 		ctx.globalAlpha = tankalpha;
 		if (editmode === false) {
-			drawStar(tankpointx, tankpointy, tanksize, angle(tankpointx, tankpointy, mouse.x, mouse.y), document.getElementById("color").value, tanksize/2);
+			drawStar(tankpointx, tankpointy, tanksize, (angle(tankpointx, tankpointy, mouse.x, mouse.y))-15, document.getElementById("color").value, tanksize/2);
 		} else {
 			drawStar(tankpointx, tankpointy, tanksize, 90, document.getElementById("color").value, tanksize/2);
 		}
