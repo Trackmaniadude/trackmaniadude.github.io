569cc32802429cc11900dcde0489f003a5270cc8
Trackmaniadude <alex108@tringali.org>
2017-02-02 20:19:32 -0500

    Update main.js

--- Drones.io/Scripts/main.js
+++ Drones.io/Scripts/main.js
@@ -172,7 +172,7 @@ function controls(){
 
 	//Game Keyboard Controls
 	if ((keysPressed[87] || keysPressed[38]) && yV<10){
-		yV+=2
+		yV+=1.2
 	}
 	if (keysPressed[68] || keysPressed[39]){			
 		xV+=0.8
