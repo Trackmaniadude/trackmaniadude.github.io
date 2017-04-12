f96351590cd97f90b638a854e4838ddd421aecc1
Trackmaniadude <alex108@tringali.org>
2017-04-12 11:23:28 -0400

    Update main.js

--- Fidgets/main.js
+++ Fidgets/main.js
@@ -1,1 +1,17 @@
-console.log("Script is active!");
+//Joystick
+var jX = 0
+var jY = 0
+var jD = false
+
+function joyDown() {
+  jD = true;
+}
+function joyUp() {
+  jD = false;
+}
+
+setInterval(function(){
+  if (jD) {
+    document.getElementById("joystick").style.transform = "translate("+jX+"px,"+jY+"px)";
+  }
+},50);
