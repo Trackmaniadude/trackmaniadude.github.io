295cfa0aa2483e06de4e2bbdee71b21e77047f6d
Trackmaniadude <alex108@tringali.org>
2017-04-13 10:34:14 -0400

    Update main.js

--- Fidgets/main.js
+++ Fidgets/main.js
@@ -1,22 +1,21 @@
 var mX = 0
 var mY = 0
+var mD = false
 function showCoords(event) {
     mX = event.clientX;
     mY = event.clientY;
+}
+function Down() {
+  mD = true;
+}
+function Up() {
+  mD = false;
 }
 
 //Joystick
 var jX = 0
 var jY = 0
-var jD = false
 var jH = false
-
-function joyDown() {
-  jD = true;
-}
-function joyUp() {
-  jD = false;
-}
 
 function joyOver() {
   jH = true;
@@ -28,7 +27,7 @@ function joyOut() {
 var joyStick = setInterval(function(){
   jX = mX;
   jY = mY;
-  if (jD && jH) {
-    document.getElementById("joystick").style.transform = "translate("+jX+"px,"+jY+"px)";
+  if (mD && jH) {
+    document.getElementById("joystick").style.transform = "translate("+(jX-100)+"px,"+(jY-100)+"px)";
   }
 },50);
